1dc3e987b1721c89f267f240edaa6ce046577cad
RomiiSteez <cortinacort@gmail.com>
2026-08-02 14:34:49 -0400

    add created-products workflow hook with necessary imports

---
+++ apps/backend/src/workflows/hooks/created-products.ts
@@ -0,0 +1,7 @@
+import { createProductsWorkflow } from "@medusajs/medusa/core-flows"
+import { StepResponse } from "@medusajs/framework/workflows-sdk"
+import { Modules } from "@medusajs/framework/utils"
+import { LinkDefinition } from "@medusajs/framework/types"
+import { BRAND_MODULE } from "../../modules/brand"
+import BrandModuleService from "../../modules/brand/service"
+
