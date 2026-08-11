749abfbbfe79e7fca79ba2a0059e6df48a037fea
RomiiSteez <cortinacort@gmail.com>
2026-08-10 20:16:50 -0400

    add get method to query the newly link brands to product.

--- apps/backend/src/api/admin/brands/route.ts
+++ apps/backend/src/api/admin/brands/route.ts
@@ -15,3 +15,14 @@ export const POST = async (
 
   res.json({ brand: result });
 };
+
+export const GET = async (req: MedusaRequest, res: MedusaResponse) => {
+  const query = req.scope.resolve("query");
+
+  const { data: brands } = await query.graph({
+    entity: "brand",
+    fields: ["*", "products.*"],
+  });
+
+  res.json({ brands });
+};
